767106df3736dd2deb011cb4990abfc01d3c2361
Gilson Dias Dutra Filho <gilsinho.1992@gmail.com>
2019-06-03 20:42:31 -0300

    alteracao no arquivo index.php (frontend)

--- main-banner.php
+++ main-banner.php
@@ -8,24 +8,26 @@ $quant = 2;
 $result = $car->ConsultarLimit($quant);
 
 while ($retorno = $result->fetchObject()) { ?>
-
-
-	<div class="jumbotron text-center" style="background-image: url('<?php echo $retorno->foto1?>'); 
+	<div class="main">
+		<div class="content-box text-center font-weight-bold" style="background-image: url('<?php echo $retorno->foto1?>'); 
 	background-size: cover;
 	background-position: 70%;">
-	<div class="container">
-		<h1 class="display-4 text-danger font-weight-bold"> <?php echo strtoupper($marca = $retorno->marca).' '.
-		ucwords($modelo = $retorno->modelo) ?> </h1>
-		<div class="banner2">
-			<a href="#">
-				<div class="price"><?php echo '$ '.number_format($vlcar = $retorno->valor, 0,",","."); ?></div>
-			</a>
+				<span class="text-danger bg-dark" style="display: block; width: 100%;"> <?php echo strtoupper($marca = $retorno->marca) .' '. ucwords($modelo = $retorno->modelo).' - '. $retorno->ano_fab.'/'. $retorno->ano_mod ?> </span>
+			<div class="wrap">
+				<div class="banner2">
+					<a href="#">
+						<div class="price"> <?php echo '$ '.number_format($vlcar = $retorno->valor, 0,",","."); ?> </div>
+					</a>
+				</div>
+			</div>
 		</div>
 	</div>
-</div>
+
+
+
 <?php
 
 }
-
+//
 
 ?>
